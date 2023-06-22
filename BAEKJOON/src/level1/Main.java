de8bfaf05e562e2c6fcef526b9fe7403a5991a4b
noheul1030 <papooa5140@gmail.com>
2023-06-22 20:12:59 +0900

    Update Main.java

--- BAEKJOON/src/level1/Main.java
+++ BAEKJOON/src/level1/Main.java
@@ -4,18 +4,12 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = 0;
-		
-		
-		System.out.println(A* (B%10));
-		C = B/10;
-		System.out.println(A* (C%10));
-		C = C/10;
-		System.out.println(A*C);
-		System.out.println(A*B);
+		try (Scanner sc = new Scanner(System.in)) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			int C = sc.nextInt();
+			
+			System.out.println(A+B+C);
+		}
 	}
 }
