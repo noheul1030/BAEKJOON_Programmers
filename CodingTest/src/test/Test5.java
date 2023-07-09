c88b7d9ff7849eec7442ff5d657146d30f8d14aa
noheul1030 <papooa5140@gmail.com>
2023-07-09 21:34:53 +0900

    Create Test5.java

---
+++ CodingTest/src/test/Test5.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Test5 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] random() {
+        int[] random = new int[10];
+        for (int i = 0; i < random.length; i++) {
+            random[i] = (int) Math.random() + 9;
+        }
+        return random;
+    }
+
+    // Äü Á¤·Ä(Quick sort)
+    public static void quick(int[] array) {
+
+    }
+
+}
