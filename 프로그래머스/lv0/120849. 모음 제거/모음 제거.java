9fd08cb9c80c3944360364595d6049fb1070cc81
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-07-11 22:13:45 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,6 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        return my_string.replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "");
+    }
+}
