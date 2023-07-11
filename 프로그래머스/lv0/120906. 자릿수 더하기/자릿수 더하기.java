b08965238a29df048a36ee09ef3ec60c7a695a8d
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-07-11 22:10:41 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(true){
+            answer += n % 10;
+            n /= 10;
+            if(n == 0){
+                break;
+            }
+        }
+        return answer;
+    }
+}
