0a47e8123f5b0db7c5e05a61c96eacb062be425d
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-07-24 01:28:48 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,13 +1,9 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        
-        int[] arr1 = Arrays.copyOfRange(num_list, n, num_list.length);
-        int[] arr2 = Arrays.copyOfRange(num_list, 0, n);
+        int[] copy = Arrays.copyOf(num_list, num_list.length * 2);
 
-        int[] answer = Arrays.copyOf(arr1, arr1.length + arr2.length);
-        System.arraycopy(arr2, 0, answer, arr1.length, arr2.length);
-
-        return answer;
+        System.arraycopy(num_list, 0, copy, num_list.length, num_list.length);
+        return Arrays.copyOfRange(copy, n, n + num_list.length);
     }
 }
