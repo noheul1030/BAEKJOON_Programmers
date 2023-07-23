e5d7f6745f95462ccb3cbc7a39a944119ba798d6
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-07-24 00:37:51 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        if(a % 2 == 1 && b % 2 == 1){
+            answer = (a*a)+(b*b);
+        }else if(a % 2 == 0 && b % 2 == 0){
+            answer = Math.abs(a - b);
+        }else{
+            answer = 2 * (a + b);
+        }
+        return answer;
+    }
+}
