eac7034d65e07e2cd0d01ee75d8b0439b12e8a7a
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-07-23 23:50:52 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.12 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(String myString, String pat) {
-        myString = myString.replaceAll("A","b");
-        myString = myString.replaceAll("B","a");
+        myString = myString.replaceAll("A","b").replaceAll("B","a");
         myString = myString.toUpperCase();
         if(myString.contains(pat)) return 1;
         int answer = 0;
