83f2afa414e218593d1beb764f463602df100c82
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-07-23 22:04:15 +0900

    [unrated] Title: n 번째 원소부터, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -2,13 +2,6 @@ import java.util.*;
 import java.util.stream.*;
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        List<Integer> list = new ArrayList<>();
-        for(int i = n-1; i < num_list.length; i++){
-            list.add(num_list[i]);
-        }
-        int[] answer = list.stream().mapToInt(i -> i).toArray();
-        
-        
-        return answer;
+        return Arrays.copyOfRange(num_list, n - 1, num_list.length);
     }
 }
