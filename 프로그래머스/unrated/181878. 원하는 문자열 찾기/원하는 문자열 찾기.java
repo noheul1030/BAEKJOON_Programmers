6a88f061500bf52d8e25da1f800f01a262ccc35b
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-07-24 00:31:07 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 5.32 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        myString = myString.toUpperCase();
+        pat = pat.toUpperCase();
+        if(myString.contains(pat)) return 1;
+        int answer = 0;
+        return answer;
+    }
+}
