cb2d86dd9b228ceaa4ce1da09a499537bdd54ed1
noheul1030 <papooa5140@gmail.com>
2023-07-28 21:14:04 +0900

    Update Test5.java

--- CodingTest/src/test/Test5.java
+++ CodingTest/src/test/Test5.java
@@ -8,11 +8,42 @@ public class Test5 {
 
         int[] A = { 6, 9, 5, 1, 12, 4, 8, 7, 2, 11, 10, 3 };
         
-        System.out.println(solution(A));
-    	
+        //System.out.println(solution(A));
+        
+        int[] arr = {1, 2, 3, 100, 99, 98};
+        System.out.println(solution(arr));
     }
-    public static double solution(int[] numbers) {
-    	// 배열값들의 평균 구하고 null일 경우 0 반환
-    	return Arrays.stream(numbers).average().orElse(0);
-    }    
+//    public static double solution(int[] numbers) {
+//    	// 배열값들의 평균 구하고 null일 경우 0 반환
+//    	return Arrays.stream(numbers).average().orElse(0);
+//    }    
+    
+    public static int solution(int[] arr) {
+        int answer = 0;
+        
+        int cnt = 0;
+        int count = 0;
+        while(true){
+        	cnt++;
+            for(int i = 0; i < arr.length; i++){
+                int check = arr[i];
+                if(check >= 50 && check % 2 == 0){
+                    if(arr[i] != check / 2) count++;
+                    arr[i] = check / 2;
+                    
+                }else if(check < 50 && check % 2 == 1){
+                    if(arr[i] != check * 2 + 1) count++;
+                    arr[i] = check * 2 + 1;
+                }
+            }
+            
+            if(count== 0)break;
+            else count = 0;
+        }
+        System.out.println("cnt : " + cnt);
+        if(arr[cnt]-2 == arr[cnt]-1) answer = cnt-2;
+        
+        System.out.println(answer);
+        return answer;
+    }
 }
