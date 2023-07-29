5177062b81384e225167fbe1cada73a2b10c738d
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-07-30 02:28:10 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -5,11 +5,11 @@ class Solution {
         
         for(int i = 0; i < str_list.length; i++){
             if(str_list[i].equals("l")){
-                answer =  Arrays.copyOfRange(str_list,0,i);
+                answer =  Arrays.copyOfRange(str_list, 0, i);
                 break;
             }
             if(str_list[i].equals("r")){
-                answer = Arrays.copyOfRange(str_list,i+1,str_list.length);
+                answer = Arrays.copyOfRange(str_list, i + 1, str_list.length);
                 break;
             }
         }
