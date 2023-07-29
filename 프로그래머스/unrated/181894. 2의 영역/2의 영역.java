ec94173423de742b5f81e5d61a0925b6a417d1de
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-07-30 01:30:45 +0900

    [unrated] Title: 2의 영역, Time: 1.49 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
@@ -1,8 +1,8 @@
 import java.util.*;
-import java.util.stream.*;
 class Solution {
     public int[] solution(int[] arr) {
         List<Integer> list = new ArrayList<>();
+        
         for(int i = 0; i < arr.length; i++){
             if(arr[i] == 2) list.add(i);
         }
