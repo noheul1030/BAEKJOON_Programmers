07b114af9e2bb3695134a3b60265a678c57f11fb
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-08-01 01:54:15 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,5 +1,5 @@
 select to_number(to_char(datetime,'fmhh24')) hour, count(*) count 
 from animal_outs
-where to_number(to_char(datetime,'fmhh24')) >= 9 and to_number(to_char(datetime,'fmhh24')) <= 19
 group by to_number(to_char(datetime,'fmhh24'))
-order by to_number(to_char(datetime,'fmhh24'))
+having to_number(to_char(datetime,'fmhh24')) BETWEEN 9 AND 19
+order by hour
