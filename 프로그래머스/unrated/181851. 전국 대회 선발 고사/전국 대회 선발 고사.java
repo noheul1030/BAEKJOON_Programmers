3a899272f94c3ea72ed06bf7f6831ee499b226e4
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-08-01 01:11:12 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 13.48 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        return recursion(rank, attendance, 0, 1, 0);
+    }
+    
+    public int recursion(int[] rank, boolean[] attendance, int cnt, int num, int answer){
+        System.out.println("cnt : "+ cnt + ", num : " + num + ", answer : " + answer);
+        if(cnt == 3) return answer;
+        
+        for(int i = 0; i < rank.length; i++){
+            if(num == rank[i] && attendance[i] == true){
+                if(cnt == 0) answer += 10000 * i;
+                if(cnt == 1) answer += 100 * i;
+                if(cnt == 2) answer += i;
+                
+                return recursion(rank, attendance, cnt+1, num+1, answer);
+            }
+        }
+        return recursion(rank, attendance, cnt, num+1, answer);
+    }
+}
