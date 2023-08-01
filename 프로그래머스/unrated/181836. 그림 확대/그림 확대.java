e42a1a16fcf90b9a5e632b315007b9bcd89fe3a4
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-08-01 09:34:44 +0900

    [unrated] Title: 그림 확대, Time: 1.33 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.java
@@ -1,17 +1,18 @@
 class Solution {
     public String[] solution(String[] picture, int k) {
         String[] answer = new String[picture.length * k];
-
+        
         int count = 0;
-        for(String str : picture){
-            String ss = "";
-            for(String s : str.split("")){
+        for(String arr : picture){
+            String pic = "";
+            
+            for(String str : arr.split("")){
                 for(int i = 0; i < k; i++){
-                    ss += s;
+                    pic += str;
                 }
             }
             for(int j = 0; j < k; j++){
-                answer[count] = ss;
+                answer[count] = pic;
                 count++;
             }
         }
