e5eed8fdd43b5d96cf7e25b82189f4d86db3e9b8
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-08-02 09:16:37 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -3,6 +3,6 @@ class Solution {
     public int solution(int[] common) {
         int n = common[1] - common[0];
         
-        return common[2] - common[1] == n ? common[common.length-1] + n : common[common.length-1] * common[1] / common[0];
+        return common[2] - common[1] == n ? common[common.length-1] + n : common[common.length-1] * (common[1] / common[0]);
     }
 }
