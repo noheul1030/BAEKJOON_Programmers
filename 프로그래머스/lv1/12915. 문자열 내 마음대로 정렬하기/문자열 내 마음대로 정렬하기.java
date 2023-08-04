966478ee3a5a0c02a984a627039e99d421f2dd42
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-08-04 09:32:28 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.68 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = recursion(strings, n);
+        
+        return answer;
+    }
+    public String[] recursion(String[] strings, int n){
+        String[] sorted = {};
+        int count = 0;
+        for(int i = 0, j = i + 1; i < strings.length-1; i++){
+            String tmp = "";
+            if(strings[i].charAt(n) > strings[j].charAt(n)){
+                tmp = strings[j];
+                strings[j] = strings[i];
+                strings[i] = tmp;
+                count++;                
+            }if(strings[i].charAt(n) == strings[j].charAt(n)){
+                sorted = new String[]{strings[i],strings[j]};
+                Arrays.sort(sorted);
+                strings[i] = sorted[0];
+                strings[j] = sorted[1];
+            }
+            j++;
+        }
+        
+        if(count == 0)return strings;
+        return recursion(strings, n);
+    }
+}
