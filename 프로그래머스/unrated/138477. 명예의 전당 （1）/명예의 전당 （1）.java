bb7ef8765710193f7f222f3ecaea7f9d45c90fbe
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-08-04 19:15:56 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.27 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        List<Integer> list = new ArrayList<>();
+        int[] answer = new int[score.length];
+        
+        int cnt = 0;
+        while(cnt < score.length){
+            for(int i = 0; i <= cnt; i++){
+                if(list.size() < k){
+                    list.add(score[i]);
+                    Collections.sort(list);
+                    answer[i] = list.get(0);
+                }else{
+                    list.add(score[i]);
+                    Collections.sort(list);
+                    Collections.reverse(list);
+                    answer[i] = list.get(k-1);
+                }
+            }
+            cnt++;
+            list.clear();
+        }
+        return answer;
+    }
+}
