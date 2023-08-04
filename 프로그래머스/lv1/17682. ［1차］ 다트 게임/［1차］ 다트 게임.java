7209c7040c348906a2b98c8234f1016a618b7b94
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-08-04 16:43:02 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.09 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,10 +1,9 @@
 import java.util.*;
 class Solution {
     public int solution(String dartResult) {
-        int one = 0, two = 0, three = 0;
+        int[] arr = {0,0,0};
         
-        int RoundCount = 0;
-        
+        int RoundCount = 0;        
         String score = "";
         
         while(dartResult.length() > 0){
@@ -13,59 +12,40 @@ class Solution {
                 if(Character.isDigit(dartResult.charAt(1))){
                     score = dartResult.substring(0,2);
                     dartResult = dartResult.substring(2);
-                    RoundCount++;
                 }else{
                     score = dartResult.substring(0,1);
                     dartResult = dartResult.substring(1);
-                    RoundCount++;
                 }
-                // System.out.println("score : " + score);
-                continue;
+                RoundCount++;
             }else{ // 숫자 아닐 때
                 char cha = dartResult.charAt(0);
                 if(!score.equals("")){
-                    if(RoundCount == 1) one = Integer.parseInt(score);
-                    if(RoundCount == 2) two = Integer.parseInt(score);
-                    if(RoundCount == 3) three = Integer.parseInt(score);
+                    if(RoundCount == 1) arr[0] = Integer.parseInt(score);
+                    else if(RoundCount == 2) arr[1] = Integer.parseInt(score);
+                    else if(RoundCount == 3) arr[2] = Integer.parseInt(score);
                     score = "";
                 } 
                 
-                if(RoundCount == 1){
-                    if(cha == 'S') one = one;
-                    else if(cha == 'D') one = (int)Math.pow(one,2);
-                    else if(cha == 'T') one = (int)Math.pow(one,3);
-                    if(cha == '*') one = one * 2;
-                    else if(cha == '#') one = one * -1;
-                }else if(RoundCount == 2){
-                    if(cha == 'S') two = two;
-                    else if(cha == 'D') two = (int)Math.pow(two,2);
-                    else if(cha == 'T') two = (int)Math.pow(two,3);
-
-                    if(cha == '*'){
-                        one = one * 2;
-                        two = two * 2;
-                    }else if(cha == '#'){
-                        two = two * -1;
-                    }
-                }else if(RoundCount == 3){
-                    if(cha == 'S') three = three;
-                    else if(cha == 'D') three = (int)Math.pow(three,2);
-                    else if(cha == 'T') three = (int)Math.pow(three,3);
-
-                    if(cha == '*'){
-                        two = two * 2;
-                        three = three * 2;
-                    }else if(cha == '#'){
-                        three = three * -1;
-                    }
-                }
+                SDT(arr,cha,RoundCount);
+                SDT2(arr,cha,RoundCount);
+                
                 dartResult = dartResult.substring(1);
-            
-                // System.out.println("RoundCount : " + RoundCount);
-                // System.out.println("cha : " + cha);
-                // System.out.println("one : " + one + ", two : " + two + ", three : " + three);
             }
         }
-        return one + two + three;
+        return arr[0] + arr[1] + arr[2];
+    }
+    
+    public void SDT(int[] arr, char cha, int round){
+        arr[round - 1] = cha == 'D' ? (int) Math.pow(arr[round - 1],2) : cha == 'T' ? (int)Math.pow(arr[round - 1],3) : arr[round - 1];
+    }
+    public void SDT2(int[] arr, char cha, int round){
+        if(cha == '#') arr[round - 1] = arr[round - 1] * -1;
+        else if(cha == '*'){
+            if(round == 1) arr[round - 1] = arr[round - 1] * 2;
+            else {
+                arr[round - 1] = arr[round - 1] * 2;
+                arr[round - 2] = arr[round - 2] * 2;
+            }
+        }   
     }
 }
