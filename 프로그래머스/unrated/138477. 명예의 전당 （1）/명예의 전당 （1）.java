9d544e6219abf12b494748b78eec81317f818f49
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-08-04 19:17:46 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.34 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,26 +1,47 @@
 import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
-        List<Integer> list = new ArrayList<>();
         int[] answer = new int[score.length];
-        
-        int cnt = 0;
-        while(cnt < score.length){
-            for(int i = 0; i <= cnt; i++){
-                if(list.size() < k){
-                    list.add(score[i]);
-                    Collections.sort(list);
-                    answer[i] = list.get(0);
-                }else{
-                    list.add(score[i]);
-                    Collections.sort(list);
-                    Collections.reverse(list);
-                    answer[i] = list.get(k-1);
-                }
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        int temp = 0;
+
+        for(int i = 0; i < score.length; i++) {
+
+            priorityQueue.add(score[i]);
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
             }
-            cnt++;
-            list.clear();
+
+            answer[i] = priorityQueue.peek();
         }
+
+
+
         return answer;
+        
+        
+//         List<Integer> list = new ArrayList<>();
+//         int[] answer = new int[score.length];
+        
+//         int cnt = 0;
+//         while(cnt < score.length){
+//             for(int i = 0; i <= cnt; i++){
+//                 if(list.size() < k){
+//                     list.add(score[i]);
+//                     Collections.sort(list);
+//                     answer[i] = list.get(0);
+//                 }else{
+//                     list.add(score[i]);
+//                     Collections.sort(list);
+//                     Collections.reverse(list);
+//                     answer[i] = list.get(k-1);
+//                 }
+//             }
+//             cnt++;
+//             list.clear();
+//         }
+//         return answer;
     }
 }
