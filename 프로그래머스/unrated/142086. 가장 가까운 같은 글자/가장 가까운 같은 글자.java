77bc2707b78aeee43e8a4f9831394a8d7f61823c
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-08-04 10:05:35 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.99 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,23 +1,33 @@
 import java.util.*;
 class Solution {
     public int[] solution(String s) {
-        List<Character> list = new ArrayList<>();
-        int[] answer = new int[s.length()];        
-        
-        for(int i = 0; i < s.length(); i++){
-            if(!list.contains(s.charAt(i))){
-                list.add(s.charAt(i));
-                answer[i] = -1;
-            }else{
-                int count = 1;
-                for(int j = i-1; j >= 0; j--){
-                    if(s.charAt(i) == s.charAt(j)){
-                        answer[i] = count;
-                        break;
-                    }else count++;
-                }
-            }
+        int[] answer = new int[s.length()];
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<s.length();i++){
+            char ch = s.charAt(i);
+            answer[i] = i-map.getOrDefault(ch,i+1);
+            map.put(ch,i);
         }
+        
+        
+        
+//         List<Character> list = new ArrayList<>();
+//         int[] answer = new int[s.length()];        
+        
+//         for(int i = 0; i < s.length(); i++){
+//             if(!list.contains(s.charAt(i))){
+//                 list.add(s.charAt(i));
+//                 answer[i] = -1;
+//             }else{
+//                 int count = 1;
+//                 for(int j = i-1; j >= 0; j--){
+//                     if(s.charAt(i) == s.charAt(j)){
+//                         answer[i] = count;
+//                         break;
+//                     }else count++;
+//                 }
+//             }
+//         }
         return answer;
     }
 }
