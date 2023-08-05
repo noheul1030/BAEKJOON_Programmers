5b72c0b2b57092cafea34d2074dfcaaf09ff3aef
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-08-05 16:55:07 +0900

    [unrated] Title: 과일 장수, Time: 0.32 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -2,23 +2,13 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
-        for(int i : score){
-            list.add(i);
+       
+        Arrays.sort(score);
+
+        for(int i = score.length; i >= m; i -= m){
+            answer += score[i - m] * m;
         }
-        Collections.sort(list);
-        Collections.reverse(list);
-        
-        for(int i = 0; i< score.length; i++){
-            score[i] = list.get(i);
-        }
-        
-        for(int i = 0,j = 0; i < score.length; i+=m){
-            if(i+m <= score.length){
-                int[] min = Arrays.copyOfRange(score,i,i+m);
-                answer += Arrays.stream(min).min().getAsInt() * m;
-            }
-        }
+
         return answer;
     }
 }
