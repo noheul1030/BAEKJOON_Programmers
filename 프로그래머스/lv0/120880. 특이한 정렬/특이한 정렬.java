47e80f4f6cd30d4cd13b899d07d5c94c4e1edb3b
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-08-20 23:56:29 +0900

    [level 0] Title: 특이한 정렬, Time: 0.34 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -3,7 +3,7 @@ class Solution {
     public int[] solution(int[] numlist, int n) {
         int[] answer = new int[numlist.length];
         double[] order = new double[numlist.length];
-        
+
         for(int i = 0; i < numlist.length; i++) {
             if(numlist[i] - n < 0) 
                 // n을 뺏을 때 음수 값인 경우, +0.5를 해서 같은 거리에 있는 양수 값보다 정렬을 했을 시 뒤로 밀리도록 함 
@@ -11,17 +11,17 @@ class Solution {
             else 
                 order[i] = numlist[i] - n; 
         }
-        
+
         Arrays.sort(order); // 오름차순 정렬
-        
+
         for(int i = 0; i < numlist.length; i++) {
             if(order[i] % 1 != 0) answer[i] = n - (int)order[i];
             else answer[i] = (int)order[i] + n;
         }
-        
+
         return answer;
-        
-// 아래의 방법으로는 TEST 2 항목만을 통과하지 못했다. 이유가 뭘까?
+
+//아래의 방법으로는 TEST 2 항목만을 통과하지 못했다. 이유가 뭘까?
         
 //         List<Integer> list1 = new ArrayList<>();
 //         List<Integer> list2 = new ArrayList<>();
