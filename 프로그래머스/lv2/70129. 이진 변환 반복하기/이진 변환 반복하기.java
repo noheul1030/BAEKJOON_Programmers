fc2f0010ebedf6c75b46f1d492b2a58f57fa255e
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-09-03 12:48:10 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 75.61 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,26 +1,26 @@
 class Solution {
     public int[] solution(String s) {
-        int[] answer = new int[2];
+        int[] answer = {};
         
         String ss = "";
         int zeroCount = 0;
         int count = 0;
+        
         while(true){
             if(s.equals("1")) break;
-            if(!s.equals("1")){
+            else{
                 count++;
+                
                 for(String str : s.split("")){
                     if(str.equals("0")) zeroCount++;
                     else ss = ss + str;
                 }
+                
                 s = Integer.toString(ss.length(),2);
                 ss = "";
             }
         }
         
-        answer[0] = count;
-        answer[1] = zeroCount;
-        
-        return answer;
+        return new int[]{count,zeroCount};
     }
 }
