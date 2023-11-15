4288f55e54218c1afe9f80e06b33abed8b139e85
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-11-16 00:02:36 +0900

    [Bronze V] Title: 아스키 코드, Time: 116 ms, Memory: 12748 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char ch = sc.next().charAt(0);		
+		
+		System.out.println((int)ch);
+	}
+}
