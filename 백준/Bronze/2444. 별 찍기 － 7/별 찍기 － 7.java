6b7cfa0475b3983d67e9a59af27239eb0e0d4941
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-11-19 22:32:39 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 512 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int input = s.nextInt();
+
+        for (int i = 1; i <= input; i++) {
+            for (int j = 1; j <= input - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        for (int i = input - 1; i >= 1; i--) {
+            for (int j = 1; j <= input - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
