d0bcf547b2d62a3931ca0ffdf25b75f2eab17518
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-11-25 15:54:21 +0900

    [Bronze I] Title: 소인수분해, Time: 148 ms, Memory: 13032 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);		
+		int n = sc.nextInt();
+		
+		for(int i = 2; i <= n; i++) {
+			while(n % i == 0) {
+				System.out.println(i);
+				n /= i;			
+			}
+		}
+    }
+}
