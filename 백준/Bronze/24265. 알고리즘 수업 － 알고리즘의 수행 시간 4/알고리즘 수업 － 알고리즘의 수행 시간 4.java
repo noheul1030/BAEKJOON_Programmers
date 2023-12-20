ab9ee5c0cb36c615b7b0a3ab981dafabb2cd4690
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-12-20 12:34:39 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 112 ms, Memory: 12812 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextLong();
+		System.out.println(n * (n -1) / 2);
+        System.out.println(2);
+	}
+}
