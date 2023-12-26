fd93e19db3df0df411d32384f361a130b10a89e5
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-12-26 10:54:11 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 152 ms, Memory: 13356 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		int f = sc.nextInt();
+		
+		sc.close();
+		
+		for(int i = -999; i <= 999; i++){
+			for(int j = -999; j <= 999; j++){
+				if(a * i + b * j == c){
+					if(d * i + e * j == f){
+						System.out.println(i + " " + j);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
