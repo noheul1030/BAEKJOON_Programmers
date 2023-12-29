e9487f6585b0f25c53f3e7beb7be626a8b5de36b
noheul1030 <106413731+noheul1030@users.noreply.github.com>
2023-12-29 18:58:20 +0900

    [Bronze II] Title: 대표값2, Time: 116 ms, Memory: 12876 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+		int[] arr = new int[5];
+		int ave = 0;
+		
+		for (int i = 0; i < 5; i++) {
+			arr[i] = sc.nextInt();
+			ave += arr[i];
+		}
+		Arrays.sort(arr);
+		System.out.println(ave / 5);
+		System.out.println(arr[2]);
+    }
+}
